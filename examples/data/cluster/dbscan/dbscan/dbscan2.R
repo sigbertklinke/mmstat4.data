fe877feafd06da702844a589fb159f550fae4d5a
Sigbert Klinke <sigbert.klinke@web.de>
2022-11-05 13:37:23 +0100

    doubling app dirs stopped

--- examples/data/cluster/dbscan/dbscan/dbscan2.R
+++
@@ -1,4 +0,0 @@
-# shinyApp/inst/app/dbscan/dbscan2.R
-db  <- dbscan(x[,c(4,6)], value(input$eps), value(input$pts))
-col <- c('grey', rainbow(max(db$cluster)))
-plot(x[,c(4,6)], col=col[1+db$cluster], pch=19, asp=TRUE)
