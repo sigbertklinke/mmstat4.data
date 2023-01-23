f436f38777e31b891140dfdc7a9030f035826fd9
Sigbert Klinke <sigbert.klinke@web.de>
2023-01-23 17:52:51 +0100

    deleted double apps

--- examples/cluster/dbscan/dbscan/dbscan.R
+++
@@ -1,7 +0,0 @@
-# shinyApp/inst/app/dbscan/dbscan.R
-library("dbscan")
-library("mmstat4")
-x  <- scale(import("https://shinyapps.wiwi.hu-berlin.de/d/BANK2.sav"))
-db   <- dbscan(x[,c(4,6)], value(input$eps), value(input$pts))
-col <- c('grey', rainbow(max(db$cluster)))
-plot(x[,c(4,6)], col=col[1+db$cluster], pch=19, asp=TRUE)
