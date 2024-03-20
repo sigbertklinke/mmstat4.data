7bfa1ba74291e00a28d1f502a1f045c218e19a71
Sigbert Klinke <sigbert@wiwi.hu-berlin.de>
2024-03-20 14:25:36 +0100

    example_chisq.py with ghload

--- examples/inference/example_chisq.py
+++ examples/inference/example_chisq.py
@@ -1,8 +1,7 @@
 import pandas as pd, rpy2, rpy2.robjects.pandas2ri as rop
 from scipy.stats import chi2_contingency
 # Load the Boston Housing dataset
-rpy2.robjects.r('library("MASS")')
-boston_df = rop.rpy2py(rpy2.robjects.r["Boston"])
+boston_df = ghload("Boston200.rds")
 #
 ctab = pd.crosstab(boston_df['chas'], boston_df['rad'])
 print(ctab)
