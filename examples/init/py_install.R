0ad37bce6302a96abab3a6a71512d077404dd688
Sigbert Klinke <sigbert.klinke@web.de>
2024-04-02 17:32:39 +0200

    py_install.R adapted

--- examples/init/py_install.R
+++ examples/init/py_install.R
@@ -1,9 +1,8 @@
-# install R libraries if necessary
-install(c("reticulate", "feather"), "R")
-
-# install Python modules if necessary
-install(c("pandas", "numpy", "statsmodels", "scipy", "matplotlib", "seaborn", "stemgraphic", "autoimpute",
-          "scikit-learn", "outliers"), "py")
+# install R libraries and python modules if necessary
+install(rlibs=c("reticulate", "feather"),
+        pymods=c("pandas", "numpy", "statsmodels", "scipy", "matplotlib", "seaborn", "stemgraphic", "autoimpute",
+          "scikit-learn", "outliers")
+)
 #install(c("pandas", "numpy", "scikit-learn", "rpy2", "pyreadstat", "matplotlib", "seaborn",
 #             "plotnine", "scipy", "bootstrapped", "feather-format", "pingouin", "statsmodels",
 #             "stemgraphic", "pyod", "effectsize"), "py")
