46a0e1d5e13fc46f3aca75a4399f9ce0353e89cf
Sigbert Klinke <sigbert@wiwi.hu-berlin.de>
2024-04-03 09:24:18 +0200

    renamed init scripts

---
+++ examples/init/init_py.R
@@ -0,0 +1,9 @@
+# install R libraries if necessary
+install(c("reticulate", "feather"), "R")
+
+# install Python modules if necessary
+install(c("pandas", "numpy", "statsmodels", "scipy", "matplotlib", "seaborn", "stemgraphic", "autoimpute",
+          "scikit-learn", "outliers"), "py")
+#install(c("pandas", "numpy", "scikit-learn", "rpy2", "pyreadstat", "matplotlib", "seaborn",
+#             "plotnine", "scipy", "bootstrapped", "feather-format", "pingouin", "statsmodels",
+#             "stemgraphic", "pyod", "effectsize"), "py")
--- examples/init/py_install.R
+++
@@ -1,8 +0,0 @@
-# install R libraries and python modules if necessary
-install(rlibs=c("reticulate", "feather"),
-        pymods=c("pandas", "numpy", "statsmodels", "scipy", "matplotlib", "seaborn", "stemgraphic", "autoimpute",
-          "scikit-learn", "outliers")
-)
-#install(c("pandas", "numpy", "scikit-learn", "rpy2", "pyreadstat", "matplotlib", "seaborn",
-#             "plotnine", "scipy", "bootstrapped", "feather-format", "pingouin", "statsmodels",
-#             "stemgraphic", "pyod", "effectsize"), "py")
