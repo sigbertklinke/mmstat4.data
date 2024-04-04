5e323db97288bcdea37b8e05d43df7ee830c3697
Sigbert Klinke <sigbert.klinke@web.de>
2024-04-04 10:05:53 +0200

    example_signtest.py reworked

--- examples/univariate/example_signtest.py
+++ examples/univariate/example_signtest.py
@@ -1,12 +1,14 @@
 from scipy.stats import binom
-def sign_test(data, mu0=0.5):
-    ntie = sum(1 for x in data if x == mu0)
-    nlow = sum(1 for x in data if x < mu0)
-    nupp = sum(1 for x in data if x > mu0)
-    if (nlow<nupp):
-        nlow = nlow+ntie
+def sign_test(data, mu0=None):
+    p0 = mu0
+    if p0 is None:
+        p0 = data.mean()
+    ntie = sum(1 for x in data if x == p0)
+    nlow = sum(1 for x in data if x < p0)
+    nupp = sum(1 for x in data if x > p0)
+    if (nlow<nupp): nlow = nlow+ntie
     # Calculate the p-value using the binomial distribution
-    p_value = binom.cdf(min(nlow, len(data)-nlow), len(data))
+    p_value = 2*binom.cdf(min(nlow, len(data)-nlow), n=len(data), p=0.5)
     return p_value
 Boston = ghload("Boston200.rds")
 p_value = sign_test(Boston['medv'])
